c0a7af95358c2f1d6a7edcf4f1f349df812840d7
Derek Cerretani <derekcerretani@gmail.com>
2018-12-27 16:53:03 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
 # Write your code here.
+def badge_maker(speaker)
+  return "Hello, my name is #{speaker}."
+end
+
+def batch_badge_creator(array)
+  list = []
+  array.each do |name|
+    list.push("Hello, my name is #{name}.")
+  end
+  return list
+end
